f27c9e3a9b6ef057abbd9a140456816b31a5538e
joanna <jo.senczuk@gmail.com>
2017-11-24 13:15:57 +0000

    Create City class

---
+++ src/main/java/cityDistanceCalculator/City.java
@@ -0,0 +1,26 @@
+package cityDistanceCalculator;
+
+public class City {
+
+    private String name;
+    private final double latitude;
+    private final double longitude;
+
+    public City(String name, double latitude, double longitude) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+}
